eca51595ca5cbf00731cfcd4d1721ddcbdf07235
Alessio Napoli <alessionapoli99@icloud.com>
2022-02-07 16:46:48 +0100

    Ended Exercise

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 let container = document.getElementById("container");
 let arrNumber= [];
-let i = 0;
+let currentNumber = 0;
 // Created with cicle While 5 Random Numbers
-while(i < 5){
+while(currentNumber < 5){
   let number = randomNumber(100,1);
   if(!arrNumber.includes(number)){
     arrNumber.push(number);
-    i++
+    currentNumber++
   }    
 };
 
@@ -14,11 +14,11 @@ while(i < 5){
 let time = 30;
 
 let counterTime = setInterval(function() {
+  document.getElementById("time").innerHTML = `Hai ancora ${time}s per memorizzarli..`
   if(time == 0){
     clearInterval(counterTime);
-  }
+  };
   time--;
-  document.getElementById("time").innerHTML = `Hai ancora ${time}s per memorizzarli..`
 },1000);
 
 // Printed on screen inside the container
@@ -27,7 +27,7 @@ container.innerHTML += `<div style="background-color:#${randomColor()}" class="c
                         </div>`;
 
 // Set timer 30s and remove the element inside the container
-setTimeout ( myFunction, time * 1000);
+setTimeout ( myFunction, time * 1050);
 function myFunction (){
   container.innerHTML = "";
 };
@@ -43,3 +43,24 @@ function randomColor (){
   return Math.floor(Math.random()*11111111).toString(16);
 }
 
+
+setTimeout(function(){
+  let myNumbersIRemember = [];
+  let correctNumber = [];
+  for(let i = 0; i < arrNumber.length; i++){
+    let myNumber = parseInt(prompt(`Inserire qui il ${i + 1}° numero che ricordi:`));
+    myNumbersIRemember.push(myNumber);
+    console.log("Il mio numero" + myNumber)
+    if(arrNumber.includes(myNumber)){
+      correctNumber.push(myNumber);
+    }
+  }
+  container.innerHTML += `<span class="text">Hai indovinato ${correctNumber.length} numeri</span>
+                          <div style="background-color:#${randomColor()}" class="content">
+                            <span>${correctNumber}</span>
+                          </div>`;
+
+console.log("I numeri da ricordare sono " + arrNumber)
+console.log("I numeri che ho indovinato sono " + correctNumber)
+console.log(myNumbersIRemember)
+},32000)
